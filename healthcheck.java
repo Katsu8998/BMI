7ae1394bd633a2375af1f13772a7358da7200973
Katsu <114673266+Katsu8998@users.noreply.github.com>
2022-11-03 00:42:42 +0900

    Update healthcheck.java

--- healthcheck.java
+++ healthcheck.java
@@ -10,8 +10,17 @@
 <body>
 <h1>BMI健康診断</h1>
 <form action="/example/HealthCheck" method="post">
+名前：<input type="text" name="name"><br> 
+		男<input 	type="radio" name="gender" value="0">
+		 女<input type="radio" name="gender" value="1"> <br>
 身長:<input type ="text" name="height">(cm)<br>
 体重: <input type ="text" name = "weight">(kg)<br>
+年齢:<select 　name="age">
+			<option value="teens">10代</option>
+			<option value="youngers">20代</option>
+			<option value="adults">30代-60代</option>
+			<option value="olders">70代</option>
+		</select><br>
 
 <input type ="submit" value ="診断">
 </form>
